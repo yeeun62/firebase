863d4f6eca4d390a4c0dd00ced1c1b5aade88a1e
방예은 <bang-yeeun@bang-yeeun-ui-MacBookPro.local>
2021-11-11 13:12:00 +0900

    test;

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { getDatabase, ref, set, onValue } from "firebase/database";
+import { getDatabase, ref, set, onValue, push } from "firebase/database";
 import config from "./config";
 
 function App() {
@@ -45,6 +45,32 @@ function App() {
 		});
 	};
 
+	// Create a new post reference with an auto-generated id
+	// 고유키 생성 및 데이터 추가 함수
+	const createData = () => {
+		const db = getDatabase();
+		const dbRef = ref(db, "user/child");
+		const newdbRef = push(dbRef);
+		set(newdbRef, {
+			city: "test",
+			test: "test",
+		});
+	};
+
+	// console에 찍히게
+	onValue(
+		ref(db, "user"),
+		(snapshot) => {
+			const data = snapshot.val();
+			const postReference = snapshot.ref;
+			const postId = snapshot.key;
+			console.log("~~~", data);
+			//updateStarCount(postElement, data);
+		}
+		//? 한번만 읽기
+		//? { onlyOnce: true }
+	);
+
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
@@ -63,11 +89,7 @@ function App() {
 				/>
 				<br />
 				<input type="button" value="읽어오기" onClick={readData} />
-				<input
-					type="button"
-					value="수정하기"
-					onClick={() => updateData(city, name)}
-				/>
+				<input type="button" value="수정하기" onClick={createData} />
 			</form>
 			{state && (
 				<p>
